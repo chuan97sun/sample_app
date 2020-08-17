0304abd4f1b6734f83f5beac75eed2ee2f287378
chuan97 <chuandao123@gmail.com>
2020-08-17 00:04:36 -0700

    Second Edition

--- app/controllers/static_page_controller.rb
+++ app/controllers/static_page_controller.rb
@@ -1,7 +1,5 @@
 class StaticPageController < ApplicationController
-  def home
-  end
+  def home; end
 
-  def help
-  end
+  def help; end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
+	
   root "static_page#home"
    get "static_page/home"
    get "static_page/help"
-  
-  end 
+  end
